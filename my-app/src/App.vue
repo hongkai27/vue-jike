47940c10c03519b4a4180db7f4930a8356e3e52b
hongkai <952817116@qq.com>
2019-03-30 15:22:49 +0800

    first commit

---
+++ my-app/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <input type="text" v-model="info" >
+    <button @click="handlerclick">提交</button>
+    <ul>
+      <todu v-for="item in list" :key="item" :item="item"></todu>
+    </ul>
+  </div>
+</template>
+
+<script>
+import todu from './components/todu.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      list:[],
+      info:''
+    }
+  },
+  methods:{
+    handlerclick(){
+      this.list.push(this.info)
+      this.info = ''
+    }
+  },
+  components: {
+    todu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-app/src/components/todu.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <li>{{item}}</li>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['item']
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
